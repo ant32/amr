fb30dd8a84fb96f4bafab2b34527f89d34b4c950
ant32 <antreimer@gmail.com>
2013-08-22 13:25:24 -0600

    Update step1.sh

--- install/step1.sh
+++ install/step1.sh
@@ -1,7 +1,7 @@
-echo -e "n\n\n\n\n\nw" | fdisk /dev/sda
+echo -e 'n\n\n\n\n\nw' | fdisk /dev/sda
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
-yes | pacstrap -i /mnt base base-devel syslinux openssh nginx
+echo -e '\n\n' | pacstrap -i /mnt base base-devel syslinux openssh nginx
 genfstab -p /mnt >> /mnt/etc/fstab
 echo amr > /mnt/etc/hostname
 curl https://raw.github.com/ant32/amr/master/install/step2.sh > /mnt/root/step2.sh

b485eff7cfb967f2354cb4beec4c1c695a3eb922
ant32 <antreimer@gmail.com>
2013-08-22 14:33:07 -0600

    test headers 5904

--- update.sh
+++ update.sh
@@ -40,7 +40,7 @@ compile() {
       install_deps
       # fix download paths
       [ "$pkg" = 'mingw-w64-headers-svn' ] && curl -O 'https://gist.github.com/ant32/6295855/raw/f2fa0b172f5b6320613dc1cd0914e0697cb6b6ca/PKGBUILD'
-      [ "$pkg" = 'mingw-w64-headers-svn' ] && sed -e "s|5882|5969|g" -i PKGBUILD
+      [ "$pkg" = 'mingw-w64-headers-svn' ] && sed -e "s|5882|5904|g" -i PKGBUILD
       [ "$pkg" = 'mingw-w64-crt-svn' ] && sed -e "s|mingw-w64.svn.sourceforge.net/svnroot/mingw-w64|svn.code.sf.net/p/mingw-w64/code|g" -i PKGBUILD
       [ "$pkg" = 'mingw-w64-winpthreads' ] && sed -e "s|mingw-w64.svn.sourceforge.net/svnroot/mingw-w64|svn.code.sf.net/p/mingw-w64/code|g" -i PKGBUILD
       [[ "$pkg" = *"qt5"* ]] && sed -e "s|releases.qt-project.org/qt5/|download.qt-project.org/archive/qt/5.0/|g" -i PKGBUILD
@@ -132,7 +132,7 @@ create_updatelist() {
     curver=`pacman -Si $pkg | grep Version | tr -d ' ' | sed -e "s/Version://" | head -n 1`
 
     # manual changes to some packages to make them not auto update
-    [ "$pkg" = 'mingw-w64-headers-svn' ] && [ "$nver" = '5792-2' ] && nver='5969-1'
+    [ "$pkg" = 'mingw-w64-headers-svn' ] && [ "$nver" = '5792-2' ] && nver='5904-1'
     [ "$pkg" = 'gyp-svn' ] && [ "$nver" = '1678-1' ] && nver='1701-1'
     [ "$pkg" = 'mingw-w64-gettext' ] && [ "$nver" = '0.18.2.1-1' ] && nver='0.18.3.1-1'
     

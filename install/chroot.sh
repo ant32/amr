54992ec2ccde00faf2a8ccafb0f8edbd4dbeedb4
Philip A Reimer <antreimer@gmail.com>
2013-09-21 13:04:33 -0600

    update to new update.sh

--- install/chroot.sh
+++ install/chroot.sh
@@ -1,12 +1,14 @@
 # creating packages as a user without a home folder does not always work
 useradd -m amr
 
-mkdir -p /build/scripts
-cd /build/scripts
+mkdir -p /scripts
+cd /scripts
 
+pushd /build
 curl -O https://aur.archlinux.org/packages/du/dummy/PKGBUILD
 makepkg --asroot
 rm -fR PKGBUILD src pkg
+popd
 
 curl -O https://raw.github.com/ant32/amr/master/buildlist.txt
 curl -O https://raw.github.com/ant32/amr/master/repo_update.sh
@@ -28,9 +30,11 @@ SigLevel = Optional TrustAll
 Server = https://dl.dropboxusercontent.com/u/195642432' >> /etc/pacman.conf
 
 mkdir -p /srv/http/archlinux/mingw-w64{-testing,}/os/x86_64
-cp dummy-1-1-any.pkg.tar.xz /build/scripts
-cp dummy-1-1-any.pkg.tar.xz /srv/http/archlinux/mingw-w64/os/x86_64
-cp dummy-1-1-any.pkg.tar.xz /srv/http/archlinux/mingw-w64-testing/os/x86_64
+mkdir -p /srv/http/logs
+cp /build/dummy-1-1-any.pkg.tar.xz /scripts
+cp /build/dummy-1-1-any.pkg.tar.xz /srv/http/archlinux/mingw-w64/os/x86_64
+cp /build/dummy-1-1-any.pkg.tar.xz /srv/http/archlinux/mingw-w64-testing/os/x86_64
 ./repo_update.sh
 pacman -Sy
 chown -R amr /build /srv/http/archlinux
+chown -R amr /build /srv/http/logs
